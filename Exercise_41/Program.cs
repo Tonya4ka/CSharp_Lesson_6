b3713989af645edb8c8fb65b0f7ed5caa53b1a7e
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-06 18:57:41 +0300

    Add Exercise_41

---
+++ Exercise_41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.Clear();
+Console.Write("Type a number M: ");
+int m = int.Parse(Console.ReadLine()!);
+int[] array = new int[m];
+FillArray(array);
+Console.WriteLine($"{PositiveNumbers(array)} numbers > 0");
+
+void FillArray(int[] array)
+{
+    int lenght = array.Length;
+   
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=int.Parse(Console.ReadLine());
+        //if (array == "")
+        //break;
+    }
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+            count++;
+    }
+    return count;
+}
